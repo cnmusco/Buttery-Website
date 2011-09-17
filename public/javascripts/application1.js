a9ebae8ff4783b6c92e27b6c14dd674458e09587
mike <mikenyy3@gmail.com>
2011-09-17 19:36:57 -0400

    Fixed signup and login menus

--- public/javascripts/application1.js
+++ public/javascripts/application1.js
@@ -9,6 +9,12 @@ function buttons()
 {
     $("#log_in").click(function()
     {
+        //If the other menu is showing, hide it before showing this one
+        if($("#sign_up_menu").css("display")!="none")
+        {
+            $("#sign_up_menu").hide();
+        }
+        
         if($("#log_in_menu").css("display")=="none")
         {
             $("#invalid_login").hide();
@@ -22,13 +28,45 @@ function buttons()
     
     $("#login_button").click(function()
     {
-        if($("#netid").val() && $("#pwd").val())
+        if($("#email").val() && $("#pwd").val())
         {
             $("#log_in_menu").slideUp('fast', function(){});
         }
         else
         {
             $("#invalid_login").show();
+        }
+    });
+    
+    
+     $("#sign_up_butt").click(function()
+        {
+            //If the other menu is showing, hide it before showing this one
+            if($("#log_in_menu").css("display")!="none")
+            {
+                $("#log_in_menu").hide();
+            }
+            
+            if($("#sign_up_menu").css("display")=="none")
+            {
+                $("#invalid_login1").hide();
+                $("#sign_up_menu").slideDown('fast', function(){});
+            }
+            else
+            {
+                $("#sign_up_menu").slideUp('fast', function(){});
+            }
+    });
+    
+    $("#sign_up_button").click(function()
+    {
+        if($("#email").val() && $("#pwd").val())
+        {
+            $("#sign_up_menu").slideUp('fast', function(){});
+        }
+        else
+        {
+            $("#invalid_login1").show();
         }
     });
 }
