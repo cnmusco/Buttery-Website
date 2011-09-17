a2a41d684fa3fba3e87cfa594059d5cea997511e
mike <mikenyy3@gmail.com>
2011-09-17 16:24:32 -0400

    log in drop box

--- public/javascripts/application1.js
+++ public/javascripts/application1.js
@@ -2,5 +2,29 @@
 //load after the page loads
 $(document).ready(function()
 {
+    buttons();
+    //$("#log_in_menu").hide();
+}); 
+
+function buttons()
+{
+    $("#log_in").click(function()
+    {
+        if($("#log_in_menu").css("display")=="none")
+        {
+            $("#log_in_menu").slideDown('fast', function(){});
+        }
+        else
+        {
+            $("#log_in_menu").slideUp('fast', function(){});
+        }
+    });
     
-});
+    $("#login_button").click(function()
+    {
+        if($("#netid").val() && $("#pwd").val())
+        {
+            $("#log_in_menu").slideUp('fast', function(){});
+        }
+    });
+}
