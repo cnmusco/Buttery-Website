79fd287f3aab166b8de0be2476828d7b892e6fba
cnmusco <Cameron.musco@gmail.com>
2011-10-18 13:06:24 -0400

    about toe merge Mike's stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,33 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110919025049) do
+ActiveRecord::Schema.define(:version => 20110919235033) do
+
+  create_table "ingredients", :force => true do |t|
+    t.string   "name"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "menus", :force => true do |t|
+    t.string   "name"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "parents", :force => true do |t|
+    t.string   "name"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "title"
     t.text     "content"
