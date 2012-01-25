8cdfb5141a5692a8723d74a033c394e552efabc6
mike <mikenyy3@gmail.com>
2012-01-25 16:10:36 -0500

    tracking

--- public/javascripts/application1.js
+++ public/javascripts/application1.js
@@ -11,6 +11,26 @@ $(document).ready(function()
 }); 
 
 
+function analytics()
+{
+    var str=window.location.href;
+    var y=new RegExp("/home");
+    var tmp=str.split('/');
+    if(root || str.match(y))
+    {
+         var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-28607691-1']);
+      _gaq.push(['_setDomainName', 'piersonbuttery.com']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    }
+}
+
 function see_butt_open()
 {
     var str=window.location.href;
