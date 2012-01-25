9d60c259f96e17e9f0bf36c4666780ba8d87777b
mike <mikenyy3@gmail.com>
2012-01-25 16:21:52 -0500

    a

--- public/javascripts/application1.js
+++ public/javascripts/application1.js
@@ -8,23 +8,8 @@ $(document).ready(function()
     reload_order_queue();
     buttons();
     see_butt_open();
-    analytics();
 }); 
 
-
-function analytics()
-{
-     var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-28607691-1']);
-  _gaq.push(['_setDomainName', 'piersonbuttery.com']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-}
 
 function see_butt_open()
 {
